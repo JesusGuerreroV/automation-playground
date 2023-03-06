a7fa84379aa5bf37ac84070587f9288aac1b6874
JesusGuerreroV <jesusvargasgro@gmail.com>
2023-03-06 15:41:14 -0600

    agregado CloseChromeDriver

---
+++ CloseChromeDriver.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class CloseChromeDriver {
+	
+	public static void main(String[] args) throws IOException {
+		Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe");
+		
+	}
+}
