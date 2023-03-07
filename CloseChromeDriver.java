1945ab5d499b5b66f3a4a061679efbed05d7165b
JesusGuerreroV <jesusvargasgro@gmail.com>
2023-03-07 08:54:16 -0600

    agregada sentencia para cerrar chrome

--- CloseChromeDriver.java
+++ CloseChromeDriver.java
@@ -4,6 +4,7 @@ public class CloseChromeDriver {
 	
 	public static void main(String[] args) throws IOException {
 		Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe");
+		Runtime.getRuntime().exec("taskkill /F /IM chrome.exe");
 		
 	}
 }
